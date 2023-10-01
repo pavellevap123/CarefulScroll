d731bda5b9221ff7efc707ce5dca481e9abbba7a
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-10-01 12:57:49 +0200

    Initial Commit

---
+++ CarefulScroll/CarefulScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulScrollApp.swift
+//  CarefulScroll
+//
+//  Created by Pavel Paddubotski on 1.10.23.
+//
+
+import SwiftUI
+
+@main
+struct CarefulScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
