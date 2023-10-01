c9ae866e9f09ef6687fdf186bb9f4132311cfa72
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-10-01 16:01:13 +0200

    Finish

--- CarefulScroll/ContentView.swift
+++ CarefulScroll/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    struct Const {
+        static let rectangleHeight: CGFloat = 50
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            ScrollView {
+                VStack {
+                    ForEach(1..<100) { int in
+                        Text(String(int))
+                        Spacer()
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding(.bottom, Const.rectangleHeight)
+            .overlay(alignment: .bottom) {
+                Color.cyan
+                    .opacity(0.5)
+                    .frame(height: Const.rectangleHeight)
+                    .offset(x: 0, y: 0)
+            }
+            .tabItem {
+                Label("Received", systemImage: "tray.and.arrow.down.fill")
+            }
         }
-        .padding()
     }
 }
 
